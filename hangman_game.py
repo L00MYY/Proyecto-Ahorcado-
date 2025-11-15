7e52cca96c1f62f1f9ed7038bfeba2af20354ec6
marcosflorescruz <20255114@esen.edu.sv>
2025-11-15 15:01:39 -0600

    x
    x

---
+++ hangman_game.py
@@ -0,0 +1,30 @@
+import pygame 
+import sys
+import random
+
+
+# -------- INICIO PARTE DE OSCAR -------- #
+
+# -------- FIN PARTE DE OSCAR -------- #
+
+
+
+# -------- INICIO PARTE DE HERBERTH -------- #
+
+# -------- FIN PARTE DE HERBERTH -------- #
+
+
+
+
+# -------- INICIO PARTE DE CARLOS -------- #
+
+# -------- FIN PARTE DE CARLOS -------- #
+
+
+
+# -------- INICIO PARTE DE SOSA -------- #
+
+# -------- FIN PARTE DE SOSA -------- #
+
+
+
