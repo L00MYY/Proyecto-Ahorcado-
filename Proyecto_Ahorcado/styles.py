6fa65cbc4b9d499d2ab3874bcaf0694fde9e54b9
Herberth Barrera <20255196@esen.edu.sv>
2025-11-15 20:44:09 -0600

    Carpeta styles
    Archivo con colores, fuentes y estilos

---
+++ Proyecto_Ahorcado/styles.py
@@ -0,0 +1,10 @@
+TITLE_CHAR = "="       
+SEPARATOR_CHAR = "-"  
+LOADING_CHAR = "#"     
+EMPTY_CHAR = "."     
+
+
+def center_text(text, width=80):
+    padding = (width - len(text)) // 2
+    
+    return " " * padding + text
