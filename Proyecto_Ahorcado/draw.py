59690b0f45eb0ffb54e7cf55da248444206b96e6
Carlos Quintanilla <160758553+L00MYY@users.noreply.github.com>
2025-11-18 10:38:42 -0600

    Trazado de líneas del muñeco, este es el dibujo solamente.

--- Proyecto_Ahorcado/draw.py
+++ Proyecto_Ahorcado/draw.py
@@ -1,1 +1,25 @@
-"Dibuja el muñeco y textos"
+# hangman_doll.py
+import pygame
+
+BLACK = (0, 0, 0)
+
+def draw_head(surface, x, y):
+    pygame.draw.circle(surface, BLACK, (x, y), 30, 3)
+
+def draw_body(surface, x, y):
+    pygame.draw.line(surface, BLACK, (x, y + 30), (x, y + 110), 4)
+
+def draw_left_arm(surface, x, y):
+    pygame.draw.line(surface, BLACK, (x, y + 50), (x - 40, y + 20), 4)
+
+def draw_right_arm(surface, x, y):
+    pygame.draw.line(surface, BLACK, (x, y + 50), (x + 40, y + 20), 4)
+
+def draw_left_leg(surface, x, y):
+    pygame.draw.line(surface, BLACK, (x, y + 110), (x - 30, y + 170), 4)
+
+def draw_right_leg(surface, x, y):
+    pygame.draw.line(surface, BLACK, (x, y + 110), (x + 30, y + 170), 4)
+
+
+
