28cd8443d95b04b2ecfa89d4d16b1f869e8010a0
Herberth Barrera <20255196@esen.edu.sv>
2025-11-18 19:25:47 -0600

    Interfaz de usuario para el juego

--- Proyecto_Ahorcado/styles.py
+++
@@ -1,9 +0,0 @@
-TITLE_CHAR = "="       
-SEPARATOR_CHAR = "-"   
-LOADING_CHAR = "#"     
-EMPTY_CHAR = "."       
-
-
-def center_text(text, width=80):
-    padding = (width - len(text)) // 2
-    return " " * padding + text
